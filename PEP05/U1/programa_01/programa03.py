5a0eb60199b3abd3ea240a89552deed78fdbcdd4
GuilleNy <nieblarobertguillermo@gmail.com>
2025-09-25 00:09:24 +0200

    ejercicio 4 hecho

--- PEP05/U1/programa_01/programa03.py
+++ PEP05/U1/programa_01/programa03.py
@@ -8,4 +8,4 @@ IES Leonardo Da Vini.
 Del ciclo DAW
 """
 
-print("Soy estudiante del \nIES Leonardo Da Vinci. \n\nDel ciclo DAW")
+print("Soy estudiante del \nIES Leonardo Da Vinci. \n\nDel ciclo DAW.")
