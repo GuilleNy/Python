bf6052e04c9b9acb2606c1f0ebc39fa61d74c1cb
GuilleNy <nieblarobertguillermo@gmail.com>
2025-09-24 12:37:37 +0200

    ejercicio 2 hecho

---
+++ PEP05/U1/programa_01/programa01.py
@@ -0,0 +1,5 @@
+
+
+
+
+print("Guillermo Niebla")
---
+++ PEP05/U1/programa_01/programa02.py
@@ -0,0 +1,21 @@
+
+"""
+Escribe un programa que escriba por pantalla: el booleano True, 
+dos literales enteros, dos literales flotantes y una cadena 
+de caracteres, y para cada uno de ellos su tipo de datos.
+"""
+
+
+booleano= True
+num1_int=20
+num2_int=30
+num1_float=2.34
+num2_float=4.6
+cadena_str="Buenos Dias"
+
+print(str(booleano) + " su tipo es " + str(type(booleano)))
+print(str(num1_int) + " su tipo es " + str(type(num1_int)))
+print(str(num2_int) + " su tipo es " + str(type(num2_int)))
+print(str(num1_float) + " su tipo es " + str(type(num1_float)))
+print(str(num2_float) + " su tipo es " + str(type(num2_float)))
+print(cadena_str + " su tipo es " + str(type(cadena_str)))
