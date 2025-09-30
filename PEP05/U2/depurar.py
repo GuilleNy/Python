243256d10385f21cab41cb49da320f665804a242
GuilleNy <nieblarobertguillermo@gmail.com>
2025-09-30 10:04:30 +0200

    depurar

--- PEP05/U2/depurar.py
+++ PEP05/U2/depurar.py
@@ -5,3 +5,12 @@ num_intentos = 1
 #Leer el intento del usuario como un número entero
 intento_usuario = int(input("Por favor, introduce un numero (tienes 4 oportunidades): "))
 
+while intento_usuario != numero_secreto and num_intentos < 5:
+    print("Incorrecto.")
+    intento_usuario = int(input("Por favor, introduce un número: "))
+    num_intentos = num_intentos + 1
+
+if intento_usuario == numero_secreto:
+    print("¡Correcto!")
+else:
+    print("Incorrecto. Fin del juego. ")
