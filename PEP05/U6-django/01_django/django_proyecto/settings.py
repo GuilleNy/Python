45382b516f41de41ba058ba54c51f39b1de43af8
GuilleNy <nieblarobertguillermo@gmail.com>
2026-01-20 09:55:59 +0100

    cm

--- PEP05/U6-django/01_django/django_proyecto/settings.py
+++ PEP05/U6-django/01_django/django_proyecto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paginas', #nueva app
+    'paginas' #nueva app
 ]
 
 MIDDLEWARE = [
--- PEP05/U6-django/01_django/paginas/views.py
+++ PEP05/U6-django/01_django/paginas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
 
-# Create your views here.
+
+def vista_pagina_inicio(request):
+    return HttpResponse("Hola mundo, me llamo Guillermo!")
